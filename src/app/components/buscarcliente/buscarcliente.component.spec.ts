56749381dec9afc10b389262565623a8ae303704
JoseGonzalez99 <jose.elias.gv99@gmail.com>
2022-09-04 13:09:40 -0400

    Agregue el componente de buscar cliente

---
+++ src/app/components/buscarcliente/buscarcliente.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { BuscarclienteComponent } from './buscarcliente.component';
+
+describe('BuscarclienteComponent', () => {
+  let component: BuscarclienteComponent;
+  let fixture: ComponentFixture<BuscarclienteComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ BuscarclienteComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(BuscarclienteComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/components/buscarcliente/buscarcliente.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-buscarcliente',
+  templateUrl: './buscarcliente.component.html',
+  styleUrls: ['./buscarcliente.component.css']
+})
+export class BuscarclienteComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
