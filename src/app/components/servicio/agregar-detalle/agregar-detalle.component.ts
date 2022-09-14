e5c5bc955105c88e1c90799e8f0896da7018253c
oscar <oscarleonardopedrozo@gmail.com>
2022-09-14 10:32:12 -0400

    componente para agregar observaciones + html

--- src/app/components/servicio/agregar-detalle/agregar-detalle.component.ts
+++ src/app/components/servicio/agregar-detalle/agregar-detalle.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Persona } from 'src/app/models/persona';
+import { PresentacionProducto } from 'src/app/models/presentacionProducto';
+import { DetallePostBody, Servicio } from 'src/app/models/servicio';
+import { ServicioService } from 'src/app/services/servicio.service';
 
 @Component({
   selector: 'app-agregar-detalle',
@@ -7,9 +12,49 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AgregarDetalleComponent implements OnInit {
 
-  constructor() { }
+  servicio: Servicio = new Servicio();
+  empleado: Persona = new Persona();
+  cliente: Persona = new Persona();
+  cantidad: number = 0;
+  public presentacionProductos: PresentacionProducto[] = [];
+  public presentacionProducto: PresentacionProducto = new PresentacionProducto();
+
+  constructor(private route: ActivatedRoute, private servicioService: ServicioService) { }
 
   ngOnInit(): void {
+    this.route.paramMap.subscribe( paramMap => {
+      this.servicio.idServicio = parseInt(paramMap.get('id') ?? '');
+      this.servicioService.getUnServicio(this.servicio.idServicio)
+      .subscribe((data:any)=>{
+        this.servicio = data;
+        this.empleado = data.idEmpleado;
+        this.cliente = data.idFichaClinica.idCliente;
+        console.log(data)
+      });
+
+    });
+  }
+
+  agregarDetalle() {
+
+    let detalleBody = new DetallePostBody();
+
+    detalleBody.cantidad = this.cantidad;
+    detalleBody.idPresentacionProducto = {
+      idPresentacionProducto: this.presentacionProducto.idPresentacionProducto
+    };
+    detalleBody.idServicio = {
+      idServicio: this.servicio.idServicio
+    };
+
+    this.servicioService.postDetalle(detalleBody, this.servicio.idServicio).subscribe((data: Servicio) => console.log(JSON.stringify(data)));
+  }
+
+  getPresentacionProducto(){
+    this.servicioService.getPresentacionProducto().subscribe((data:any)=>{
+      this.presentacionProducto = data.lista;
+    })
   }
 
 }
+
